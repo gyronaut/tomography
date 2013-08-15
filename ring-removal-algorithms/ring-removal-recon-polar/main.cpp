b6ab8a458070db6c396d3ac2a9d3461f6e998263
Justin Blair <jbeta137@gmail.com>
2013-08-15 16:19:20 -0700

    New ring problem came up, trying to fix

--- ring-removal-algorithms/ring-removal-recon-polar/main.cpp
+++ ring-removal-algorithms/ring-removal-recon-polar/main.cpp
@@ -6,8 +6,8 @@
 #include <cmath>
 #include "time.h"
 
-//#include "tiff_io-win.h"
-#include "tiff_io.h"
+#include "tiff_io-win.h"
+//#include "tiff_io.h"
 #include "image_transforms.h"
 #include "image_filters.h"
 
@@ -33,11 +33,11 @@ void doRingFilter(float*** polar_image, int pol_height, int pol_width, float thr
 //	filter_machine->doMedianFilterFast1D(&filtered_image, polar_image, 0, 0, pol_height-1, pol_width-1, 'x', (ring_width -1)/2, ring_width, pol_width, pol_height);	
 //	filter_machine->doMedianFilter1D(&filtered_image, polar_image, 0, 0, pol_height-1, pol_width-1, 'x', (ring_width - 1)/2, ring_width, pol_width, pol_height);
 		
-	filter_machine->doMedianFilterFast1D(&filtered_image, polar_image, 0, 0, pol_height-1, pol_width/3 -1, 'x', m_rad/3, ring_width, pol_width, pol_height);
+	filter_machine->doMedianFilterFast1D(&filtered_image, polar_image, 0, 0, pol_height-1, pol_width/3 -1, 'x', m_rad, ring_width, pol_width, pol_height);
 
 	filter_machine->doMedianFilterFast1D(&filtered_image, polar_image, 0, pol_width/3, pol_height-1, 2*pol_width/3 -1, 'x', 2*m_rad/3, ring_width, pol_width, pol_height);
 
-	filter_machine->doMedianFilterFast1D(&filtered_image, polar_image, 0, 2*pol_width/3, pol_height-1, pol_width-1, 'x', m_rad, ring_width, pol_width, pol_height);
+	filter_machine->doMedianFilterFast1D(&filtered_image, polar_image, 0, 2*pol_width/3, pol_height-1, pol_width-1, 'x', m_rad/3, ring_width, pol_width, pol_height);
 	
 	clock_t end_median = clock();
 	if(verbose == 1) printf("Time for median filter: %f sec \n", (float(end_median - start_median)/CLOCKS_PER_SEC));
@@ -61,10 +61,16 @@ void doRingFilter(float*** polar_image, int pol_height, int pol_width, float thr
 
 	if(verbose == 1) printf("Performing Azimuthal mean filter... \n");
 	clock_t start_mean = clock();
-
+/*
 	filter_machine->doMeanFilterFast1D(&filtered_image, polar_image, 0, 0, pol_height-1, pol_width/3-1, 'y', m_azi/3, pol_width, pol_height);
 	filter_machine->doMeanFilterFast1D(&filtered_image, polar_image, 0, pol_width/3, pol_height-1, 2*pol_width/3-1, 'y', 2*m_azi/3, pol_width, pol_height);
 	filter_machine->doMeanFilterFast1D(&filtered_image, polar_image, 0, 2*pol_width/3, pol_height-1, pol_width-1, 'y', m_azi, pol_width, pol_height);
+*/
+	filter_machine->doMedianFilterFast1D(&filtered_image, polar_image, 0, 0, pol_height-1, pol_width/3 -1, 'y', m_azi, ring_width, pol_width, pol_height);
+
+	filter_machine->doMedianFilterFast1D(&filtered_image, polar_image, 0, pol_width/3, pol_height-1, 2*pol_width/3 -1, 'y', 2*m_rad/3, ring_width, pol_width, pol_height);
+
+	filter_machine->doMedianFilterFast1D(&filtered_image, polar_image, 0, 2*pol_width/3, pol_height-1, pol_width-1, 'y', m_azi/3, ring_width, pol_width, pol_height);
 
 	clock_t end_mean = clock();
 	if(verbose == 1) printf("Time for mean filtering: %f sec\n", (float(end_mean-start_mean)/CLOCKS_PER_SEC));
